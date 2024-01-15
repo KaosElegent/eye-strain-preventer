36811dcaa2d3ec8487b49d52dc7c09f5c2d0e6b0
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-01-14 23:53:14 -0500

    basic code updated with working camera code

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,29 @@
 #include "opencv2/opencv.hpp"
-#include <opencv2/highgui/highgui.hpp>
 #include "iostream"
 
-int showImage() {
-    std::string path = "../img/test.jpeg";
-    cv::Mat img = cv::imread(path);
-    imshow("Portrait", img);
-    cv::waitKey(0);
-    return 0;
-}
+using namespace std;
 
-int showWebCameraContent() {
-    // open the first webcam plugged in the computer
-    cv::VideoCapture camera(0);
-    if (!camera.isOpened()) {
-        std::cerr << "ERROR: Could not open camera" << std::endl;
-        return 1;
+int main(int argc, char** argv) {
+    cv::namedWindow("testing", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture cap;
+
+    cap.open(0);
+
+    if(!cap.isOpened()){
+        cerr << "couldn't open capture." << endl;
+        return -1;
     }
 
-    // create a window to display the images from the webcam
-    cv::namedWindow("Webcam");
-
-    // this will contain the image from the webcam
     cv::Mat frame;
-    
-    // display the frame until you press a key
-    while (1) {
-        // capture the next frame from the webcam
-        camera >> frame;
-        // show the image on the window
-        cv::imshow("Webcam", frame);
-        // wait (10ms) for a key to be pressed
-        if (cv::waitKey(10) >= 0) {
-            break;
-        }
-    }
-    return 0;
-}
 
-int main(int, char**) {
-   showImage();
-   // showWebCameraContent();
+    while (true){
+        cap >> frame;
+        if(frame.empty()) break;
+
+        cv::imshow("testing",frame);
+        if(cv::waitKey(30) >= 0) break;
+    }
+
+
+    return 0;
 }
