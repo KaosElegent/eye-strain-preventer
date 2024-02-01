ad55df85b3569b95d45285b75ee96211515fa0bf
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-01-31 23:47:13 -0500

    altered the code to take snapshots at every interval

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,28 @@
-#include "opencv2/opencv.hpp"
-#include "iostream"
+#include <stdlib.h>  // To notify the user
+#include <unistd.h> // To put the program to sleep
+#include <iostream> // For console debugging
+#include "opencv2/opencv.hpp" // To detect the user
+
 
 using namespace std;
 
 int main(int argc, char** argv) {
+    system("notify-send ['EyeCare Assistant'] 'Look away from your screen!'");
     cv::VideoCapture cap;
     cv::CascadeClassifier face_cascade;
-    
-    cap.open(0);
     face_cascade.load("../data/haarcascade_frontalface_default.xml");
-
-    if(!cap.isOpened()){
-        cerr << "couldn't open capture." << endl;
-        return -1;
-    }
 
     cv::Mat frame;
     vector<cv::Rect> faces;
 
     while (true){
+
+        cap.open(0);
+        if(!cap.isOpened()){
+            cerr << "couldn't open capture." << endl;
+            return -1;
+        }
+
         cap >> frame;
         if(frame.empty()) break;
 
@@ -32,6 +36,8 @@ int main(int argc, char** argv) {
 
         cv::imshow("testing",frame);
         if(cv::waitKey(30) >= 0) break;
+        cap.release();
+        sleep(30);
     }
 
 
